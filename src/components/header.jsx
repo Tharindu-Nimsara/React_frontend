e2a860d51d9caf43cb4c8e01db7340e0c206c487
Tharindu Nimsara <tharinduonline1080@gmail.com>
2025-05-06 10:37:36 +0530

    login page transparent UI

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import UserData from "./userData";
 
 export default function Header(){
     return(
-        <div id="header" className="bg-green-300 flex justify-center items-center w-full h-16">
+        <div id="header" className="bg-white-300 flex justify-center items-center w-full h-16">
             
             <Link to="/login" className="text-blue-500 hover:text-blue-700 m-5">Login</Link> 
             <Link to="/signup" className="text-blue-500 hover:text-blue-700 m-5">Signup</Link>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,12 @@
 export default function LoginPage(){
     return(
-        <div className="w-full h-screen bg-amber-100 flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-bold mb-4">Login</h1>
-            <form className="bg-white p-6 rounded shadow-md w-80">
+        <div className="w-full h-screen bg-[url(login.jpg)] bg-center bg-cover backdrop-blur-md flex flex-col items-center justify-center">
+            
+            <form className=" px-10 py-8 rounded-[20px] backdrop-blur-md shadow-xl w-80">
+                <div className="flex justify-center items-center ">
+                    <h1 className="text-2xl font-bold mb-4 item-center justify-center">Login</h1>
+                </div>
+                 
                 <div className="mb-4">
                     <label className="block text-gray-700 mb-2" htmlFor="username">Username</label>
                     <input className="border border-gray-300 p-2 w-full rounded" type="text" id="username" name="username" required />
