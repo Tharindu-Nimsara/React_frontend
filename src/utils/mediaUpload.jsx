86a7b6926ca33928572127a627ffc707a0125ce8
Tharindu <tharinduonline1080@gmail.com>
2025-07-22 00:00:09 +0530

    security

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 // this file is used to upload images to supabase storage
 // and get the public url of the image
 
-const url = "https://avcszskxqccnkeybnvqh.supabase.co" 
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2Y3N6c2t4cWNjbmtleWJudnFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMDgxMDEsImV4cCI6MjA2MzU4NDEwMX0.9c3s9ostJO1jVV9BzV6IdyHrDu2cLNk4F7avTUAFkeI"
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(url, key); 
 
